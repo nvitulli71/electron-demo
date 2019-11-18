f4f021b83397bbe9cd246220558f033e313028ea
Nick Vitulli <nvitulli71@gmail.com>
2019-11-18 07:51:07 -0800

    some demo stuff

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const electron = require("electron");
 const { app, BrowserWindow } = electron;
 
+let mainWindow;
+
 app.on("ready", () => {
   //Here is where we will start stuff when electron is ready
-  new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
 });
